9118500b49f79d55b2da685c2a6126869b93a9c1
Robert Schofield <rschofield1992@gmail.com>
2024-04-09 00:31:01 -0400

    commit

--- ConsoleE/Program.cs
+++ ConsoleE/Program.cs
@@ -1,60 +1,47 @@
 ﻿namespace ConsoleE
 {    public class Program
     {
-        public static int AreaOfCircle(int radius1, int double2)
-        {
-            var answer1 = radius1 * double2;
-            return answer1;
-        }
+
+        public static double AreaOfCircle(double radius)
+        { return Math.PI * Math.Pow(radius, 2); }
 
 
-        public static int AreaOfTriangle(double half1, int base1, int height1)
-        {
+        public static double AreaOfTriangle(double baseLength, double Height)
+        { return 0.5 * baseLength * Height; }
 
-            var answer2 = half1 * base1 * height1;
-            return (int)answer2;
-        }
+        public static double AreaOfRectangle(double Length, double Width)
+        { return Length * Width; }
 
-        public static int AreaOfRectangle(int Length1, int Height1)
-        {
-            var answer3 = Length1 * Height1;
-            return answer3;
-        }
-
-        public static int AreaOfSquare(int Side1, int Double1)
-        {
-            var answer4 = Side1 * Double1;
-            return answer4;
-        }
+        public static double SquareArea(double Side)
+        { return Math.Pow(Side, 2); }
 
 
         static void Main(string[] args)
         {
-            int answer1 = AreaOfCircle(6, 6);
-            Console.WriteLine(answer1);
-            int answer2 = AreaOfTriangle(.5, 4, 2);
-            Console.WriteLine(answer2);
-            int answer3 = AreaOfRectangle(10, 5);
-            Console.WriteLine(answer3);
-            int answer4 = AreaOfSquare(5, 5);
-            Console.WriteLine(answer4);
 
-            //area of circle : 36
-            //area of triangle :4
-            //area of rectangle :50
-            //area of square : 25
+            Console.Write("Enter the radius of the circle: ");
+            double circleRadius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The area of the circle is : {AreaOfCircle(circleRadius)}");
 
-            Console.WriteLine($"Hello, what is the area of a circle, with a radius of 6?");
-            _ = Console.ReadLine();
-            Console.WriteLine($"Okay, what is the area a triangle with the base of 4 and height of 2? o");
-            _ = Console.ReadLine();
-            Console.WriteLine($"Good!, What is the area of a rectangle with a length of 10 and width of 5?");
-            _ = Console.ReadLine();
-            Console.WriteLine($"Anddd what is the area of a sqaure with a side of 5?");
-            _ = Console.ReadLine();
+            Console.WriteLine("Enter the base of the triangle");
+            double baseLength = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the height of the triangle");
+            double Height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The area of the triangle with is {AreaOfTriangle(baseLength, Height)}");
 
+            Console.WriteLine("Enter the lenght of the rectangle");
+            double Length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the width of the rectangle");
+            double Width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The area of the rectangle is {AreaOfRectangle(Length, Width)}");
+
+            Console.WriteLine("Enter the lenght of the square");
+            double Side = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The area of the square  is {SquareArea(Side)}");
 
 
         }
-    }      
+
+    }    
+         
 }
